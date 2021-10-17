7e5a9105f43ed8005e7208970d388bf8ea1fb113
sakasegawajunki <80205010+sakasegawajunki@users.noreply.github.com>
2021-10-17 10:22:31 +0900

    繰り返し処理を行おう - for構文

---
+++ 3-7.php
@@ -0,0 +1,13 @@
+<?php
+$color = ["黒", "白", "赤", "黄"];
+
+// echo $color[0];
+// echo "\n";
+// echo $color[1];
+// echo "\n";
+// echo $color[2];
+// echo "\n";
+
+for ($i=0; $i<count($color); $i++) {
+  echo "・", $color[$i], "\n";
+}
