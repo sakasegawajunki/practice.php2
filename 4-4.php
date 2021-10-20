52899c4369e6591e347118dcf57cb5c109166a48
sakasegawajunki <80205010+sakasegawajunki@users.noreply.github.com>
2021-10-20 11:39:04 +0900

    省略できるパラメーターを作ろう - デフォルト引数

---
+++ 4-4.php
@@ -0,0 +1,14 @@
+<?php
+function make_list($list,$head="・") {
+  foreach ($list as $key => $val) {
+    echo $head, $key, ":", $val, "\n";
+  }
+}
+
+$pref = [
+  "hokkaido" => "北海道",
+  "aomori" => "青森県",
+  "iwate" => "岩手県"
+];
+
+make_list($pref);
